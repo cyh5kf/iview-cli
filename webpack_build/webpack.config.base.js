d8ab37f95b1e58691df7b2f866927acec02c1a62
cyh5kf <1269415602@qq.com>
2017-12-02 01:18:38 +0800

    fix webpack

--- webpack_build/webpack.config.base.js
+++ webpack_build/webpack.config.base.js
@@ -1,96 +1,62 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-HtmlWebpackPlugin = require('html-webpack-plugin'),
+var path = require('path'),
+    webpack = require("webpack"),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: {
-        main: path.resolve(__dirname, '../src/main'),
-        vendor: ['vue', 'vue-router', 'vuex', 'lodash', 'iview', 'axios', 'moment', 'qs']
+        index: path.resolve(__dirname, '../src/index.js'),
+        vendor: ['react', 'react-dom', 'react-router-dom', 'underscore', 'antd', 'axios', 'history', 'moment', 'qs']
     },
     module: {
         rules: [
-            {
-                test: /\.(js|vue)$/,
-                loader: 'eslint-loader',
-                enforce: 'pre',
-                include: path.join(__dirname, '../src'),
+           {
+                test: /\.(png|jpg|gif|jpeg)$/, //处理css文件中的背景图片
+                exclude: /node_modules/,
+                loader: 'url-loader',
                 options: {
-                    formatter: require('eslint-friendly-formatter')
+                    name: './static/imgs/[name].[hash:8].[ext]',
+                    limit: '8192'//当图片大小小于这个限制的时候，会自动启用base64编码图片。减少http请求,提高性能
                 }
-            },
-            {
-                test: /.vue$/,
+            }, {
+                test: /\.(eot|woff|svg|ttf|woff2|gif|appcache)(\?|$)/,
+                exclude: /node_modules/,
                 use: [{
-                        loader: 'vue-loader',
-                        options: {
-                            loaders: {
-                                less: ExtractTextPlugin.extract({
-                                    use: ['css-loader?minimize', 'autoprefixer-loader', 'less-loader'],
-                                    fallback: 'vue-style-loader'
-                                }),
-                                css: ExtractTextPlugin.extract({
-                                    use: ['css-loader', 'autoprefixer-loader', 'less-loader'],
-                                    fallback: 'vue-style-loader'
-                                })
-                            }
-                        }
+                    loader: 'file-loader',
+                    options: {
+                        name: './static/fonts/[name].[hash:8].[ext]',
                     }
-                ]
-            },
-            {
-                test: /iview\/.*?js$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    use: ['css-loader?minimize', 'autoprefixer-loader'],
-                    fallback: 'style-loader'
-                })
-            },
-
-            {
-                test: /\.less/,
-                use: ExtractTextPlugin.extract({
-                    use: ['css-loader', 'autoprefixer-loader', 'less-loader'],
-                    fallback: 'style-loader'
-                })
-            },
-
-            {
-                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
-                loader: 'url-loader?limit=1024'
-            },
-            
-            {
-                test: /\.(html|tpl)$/,
-                loader: 'html-loader'
-            }
-        ]
+                }]
+            }]
     },
     resolve: {
-        extensions: ['.js', '.vue', '.less', '.json'],
+        //注意一下, extensions webpack2第一个不是空字符串! 对应不需要后缀的情况.
+        extensions: ['.js', '.json', '.sass', '.less', 'jsx'],
+        //模块别名定义，方便后续直接引用别名，无须多写长长的地址
         alias: {
             'src': path.resolve(__dirname, '../src'),
-            'api': path.resolve(__dirname, './src/api'),
-            'views': path.resolve(__dirname, './src/views'),
-            'common': path.resolve(__dirname, './src/common')
+            'components': path.resolve(__dirname, '../src/components'),
+            'utils': path.resolve(__dirname, '../src/utils'),
+            'api': path.resolve(__dirname, '../src/api')
         }
     },
 
     plugins: [
-
         //动态生成html插件
         new HtmlWebpackPlugin({
             // favicon:'./src/img/favicon.ico', //favicon路径
             template: path.join(__dirname, '../index.html'),
-            inject: true, //允许插件修改哪些内容，包括head与body、
+            inject: true, //允许插件修改哪些内容，包括head与body
+            minify: { //压缩HTML文件
+                removeComments: true, //移除HTML中的注释
+                collapseWhitespace: false //删除空白符与换行符
+            }
+        }),
+
+        new ExtractTextPlugin({
+            filename: "[name].[contenthash:8].css",
+            disable: false,
+            allChunks: true
         }),
 
         // 提取公共模块
@@ -101,4 +67,4 @@ module.exports = {
         //  Webpack 3中提供了如下的插件来允许开发者启用作用域提升特性来避免这种额外的性能损耗
         new webpack.optimize.ModuleConcatenationPlugin()
     ]
-};
+}
